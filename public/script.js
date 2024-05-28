40b35ce57e0066f420efb90b2000e41aeea46a2d
vivek <vivekranjan8789@gmail.com>
2024-05-28 12:52:03 +0530

    Added some more basic setup of socket

--- public/script.js
+++ public/script.js
@@ -2,13 +2,17 @@ var socket = io();
 
 
 let btn = document.getElementById('btn');
-btn.onclick = function() {
-    socket.emit('from_client');
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msglist')
+
+btn.onclick = function exec() {
+    socket.emit('msg_send', {
+        msg: inputMsg.value
+    });
 }
-socket.on('from_server', () => {
-    console.log("collected a new event from server");
-    const div = document.createElement('div');
-    div.innerText = 'New event from server';
-    console.log(div);
-    document.body.appendChild(div);
+
+socket.on('msg_rcvd', (data) => {
+     let limsg = document.createElement('li');
+     limsg.innerText = data.msg;
+     msgList.appendChild(limsg);
 })
